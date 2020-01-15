9fd1c5868ea74b4c7daff01d8dfc37e81a94cfe1
Zeliha Gencel <zelihagencel@gmail.com>
2020-01-15 01:23:07 -0600

    starter template of materialized downloaded added nav bar, slider pictures, cards that represent projects are done

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function ($) {
+    $(function () {
+
+        $('.sidenav').sidenav();
+
+
+
+        $('.slider').slider({
+            fullWidth: true,
+            indicators: false,
+            height: 500
+        });
+
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
