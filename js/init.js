892c9d1acb0b8ee7b334d443c56487fb78714052
Zeliha Gencel <zelihagencel@gmail.com>
2020-02-09 19:20:00 -0600

    projects are updated

--- js/init.js
+++ js/init.js
@@ -3,19 +3,14 @@
 
         $('.sidenav').sidenav();
 
-
-
         $('.slider').slider({
             fullWidth: true,
             indicators: false,
             height: 500
         });
 
-
-
         $('.scrollspy').scrollSpy({
         });
 
-
-    }); // end of document ready
+    });
 })(jQuery); // end of jQuery name space
